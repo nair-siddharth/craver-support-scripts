a6b2b66c48eefc5bb62f7f63a3b0a960fc1829ec
nair-siddharth <70758206+nair-siddharth@users.noreply.github.com>
2022-08-20 20:40:23 -0700

    GetUsers - Use JSON for DB Connection

--- Read-Info-from-DB/GetUsersInfo.py
+++ Read-Info-from-DB/GetUsersInfo.py
@@ -2,57 +2,60 @@ import mysql.connector
 import requests
 import time
 import csv
+import json
 
-#inputs
+# inputs
 # Check for company info
-#exec(open('Read-Info-from-DB/GetCompanyInfo.py').read())
-#db connection
+# exec(open('Read-Info-from-DB/GetCompanyInfo.py').read())
+# db connection
+db_connection_file = open('db-connection.json')
+db_connection = json.loads(db_connection_file)
 proddb = mysql.connector.connect(
-  host=input('Please enter host IP - '),
-  user=input('Please enter user name - '),
-  password=input('Please enter your password - '),
-  database = input('Please enter the schema name - ')
+  host=db_connection['host'],
+  user=db_connection['host'],
+  password=db_connection['password'],
+  database = db_connection['database']
 )
-
-if(proddb.is_connected()):
-  print("\nConnection Successful", end = ', ')
+if (proddb.is_connected()):
+  print("\nConnection Successful", end=', ')
   print('Schema = ' + proddb.database)
 
 getCursor = proddb.cursor()
-
+exit()
 companyID = input("\nPlease enter company ID - ")
 selectUsers = 'select id, name,email,telephone,points,balance,giftcard_id from users where company_fk='+companyID+';'
 getCursor.execute(selectUsers)
 users = getCursor.fetchall()
-ct=0
+ct = 0
 
-#open 
+# open csv file
 f = open('UsersList.csv', 'w')
 writer = csv.writer(f)
-rowArr  = ['ID','Name','Email','Phone','Points', 'Gift Card Balance', 'Wallet Balance']
+rowArr = ['ID', 'Name', 'Email', 'Phone', 'Points',
+    'Gift Card Balance', 'Wallet Balance']
 writer.writerow(rowArr)
-urlGiftCardBalance = 'Enter Gift Cards API Endpoint (please check with devs for more info) - '
-api_token = 'Enter api_token (please check with devs for more info) - '
+urlGiftCardBalance = input(
+    'Enter Gift Cards API Endpoint (please check with devs for more info) - ')
+api_token = input('Enter api_token (please check with devs for more info) - ')
 for user in users:
-	giftCard = user[6]
-	ct=ct+1
+  giftCard = user[6]
+  ct = ct+1
 
-	if(giftCard):
-		urlGiftCardBalance = urlGiftCardBalance+giftCard
-		#print(urlGiftCardBalance)
-		
-		header = {"Authorization":"Bearer "+api_token}
-		giftCard = requests.get(urlGiftCardBalance,headers = header)
-		rowArr = [user[0],user[1],user[2],user[3],user[4],user[5],giftCard.json()['balance']]
-		time.sleep(.01)
-
-	else:
-		rowArr = [user[0],user[1],user[2],user[3],user[4],user[5],'0']
-	writer.writerow(rowArr)
+  if (giftCard):
+    header = {"Authorization": "Bearer "+api_token}
+    giftCard = requests.get(urlGiftCardBalance+giftCard, headers=header)
+    try:
+      rowArr = [user[0],user[1],user[2],user[3],user[4],user[5],giftCard.json()['balance']]
+    except:
+      rowArr = [user[0],user[1],user[2],user[3],user[4],user[5],'0']
+  else:
+    rowArr = [user[0],user[1],user[2],user[3],user[4],user[5],'0']
+  writer.writerow(rowArr)
+  time.sleep(.1)
 
 f.close()
 
-#close connection
+# close connection
 proddb.close()
 if(not proddb.is_connected()):
   print("=====================================================================================================================================================")
@@ -60,5 +63,5 @@ if(not proddb.is_connected()):
 
 
 
-	
+  
 
