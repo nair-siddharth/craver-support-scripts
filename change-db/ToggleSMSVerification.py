7ff72722ef3235cc6c69646df762135ca5d6deb2
nair-siddharth <70758206+nair-siddharth@users.noreply.github.com>
2022-08-23 17:23:23 -0700

    Update ToggleSMSVerification.py

--- change-db/ToggleSMSVerification.py
+++ change-db/ToggleSMSVerification.py
@@ -25,3 +25,35 @@ selectSMSInfo = 'select sms_auth_token,sms_account_sid,sms_from_number,sms_quota
 getCursor.execute(selectSMSInfo)
 SMSInfo = getCursor.fetchall()
 
+if(SMSInfo[0][0]==None):
+	if(input('Feature not active, you want to activate it ? (Y/N) - ')=='y'):
+		activateSMS = "UPDATE companies SET sms_auth_token = '5ce24bf211a446ca96ee860374ecbf9e', sms_account_sid = 'ACba7ac8553162a9f81013a9d7eb9003e8', sms_from_number = '+17782003334', sms_quota = '1000' WHERE (id = "+companyID+");"
+		getCursor.execute(activateSMS)
+		print(str(getCursor.rowcount) + ' rows affected')
+		changeCount = getCursor.rowcount
+	else:
+		exit()
+
+elif(SMSInfo[0][0]!=None):
+	if(input('Feature active, you want to deactivate it ? (Y/N) - ')=='y'):
+		deactivateSMS = "UPDATE companies SET sms_auth_token = null, sms_account_sid = null, sms_from_number = null, sms_quota = 0 WHERE (id = "+companyID+");"
+		getCursor.execute(deactivateSMS)
+		print(str(getCursor.rowcount) + ' rows affected')
+		changeCount = getCursor.rowcount
+	else:
+		exit()
+
+
+if(changeCount>0):
+  if(input(str(changeCount)+' rows will be affected, continue? - ')=='y'):
+    proddb.commit()
+  else:
+    print('Changes rolled back')
+
+proddb.close()
+
+if(not proddb.is_connected()):
+  print("=====================================================================================================================================================")
+  print("Successfully disconnected\n")
+	
+	
