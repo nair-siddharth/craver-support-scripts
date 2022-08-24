71f77dc2031c765a43e4a72a37e3351a480b0440
nair-siddharth <70758206+nair-siddharth@users.noreply.github.com>
2022-08-23 17:29:56 -0700

    Toggle Order Status Push Notifications
    Initial Commit

---
+++ change-db/ToggleOrderStatusPushNotification.py
@@ -0,0 +1,20 @@
+import mysql.connector
+import json
+
+# Check for company info
+exec(open('read-info-from-db/GetCompanyInfo.py').read())
+
+# db connection
+db_connection_file = open('common/db-connection.json')
+db_connection = json.load(db_connection_file)
+proddb = mysql.connector.connect(
+  host=db_connection['host'],
+  user=db_connection['user'],
+  password=db_connection['password'],
+  database = db_connection['database']
+)
+if (proddb.is_connected()):
+  print("\nConnection Successful", end=', ')
+  print('Schema = ' + proddb.database)
+
+getCursor = proddb.cursor()
